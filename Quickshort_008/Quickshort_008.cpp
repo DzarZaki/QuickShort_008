05632a332701db78d1def91ecc27932da06944f9
DzarZaki <dzarzaki2@gmail.com>
2023-03-30 19:09:35 +0700

    prosedur untuk meminta input dari pengguna berupa panjang elemen array dan nilai-nilai elemen array

--- Quickshort_008/Quickshort_008.cpp
+++ Quickshort_008/Quickshort_008.cpp
@@ -1,1 +1,8 @@
-/
+#include <iostream>
+using namespace std;
+
+// Array of integers to hold values
+int arr[20];
+int cmp_count = 0;	// number of comparsion
+int mov_count = 0;	// number of data movements
+int n;
