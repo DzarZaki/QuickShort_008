f53197e7fc46a7245e2d26b020bf408f7d2cffa7
DzarZaki <dzarzaki2@gmail.com>
2023-03-30 19:12:00 +0700

    prosedur untuk menukar elemen pada indeks x dengan elemen pada indeks y dalam array "arr"

--- Quickshort_008/Quickshort_008.cpp
+++ Quickshort_008/Quickshort_008.cpp
@@ -6,3 +6,37 @@ int arr[20];
 int cmp_count = 0;	// number of comparsion
 int mov_count = 0;	// number of data movements
 int n;
+
+void input() {
+	while (true)
+	{
+		cout << "Masukkan panjang elemen array: ";
+		cin >> n;
+
+		if (n <= 20)
+			break;
+		else
+			cout << "\nMaksium panjang array adalah 20" << endl;
+	}
+
+	cout << "\n------------------" << endl;
+	cout << "\nEnter Array Element" << endl;
+	cout << "\n------------------" << endl;
+
+	for (int i = 0; i < n; i++)
+	{
+		cout << "<" << (i + 1) << ">";
+		cin >> arr[i];
+	}
+}
+
+// Swaps the elemnt at index x with the element at index y
+void swap(int x, int y)
+{
+	int temp;
+
+	temp = arr[x];
+	arr[x] = arr[y];
+	arr[y] = temp;
+}
+
